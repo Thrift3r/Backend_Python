04c297be3c774bd07231e8ce967fb6a80fd9cf34
IamRubens <contacto@iamrubens.dev>
2023-04-18 00:31:06 +0200

    add middlewars and gitignore

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from routers import products
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
 
+# Configuración de CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
 app.include_router(products.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def main():
